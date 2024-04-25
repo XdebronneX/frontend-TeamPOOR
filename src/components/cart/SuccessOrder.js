9d5a0b2cc483381ff4f5cdca449fc71bfce2c5be
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-04-26 00:07:40 +0800

    Initial commit

--- src/components/cart/SuccessOrder.js
+++ src/components/cart/SuccessOrder.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import {
     Box,
     Button,
@@ -10,13 +11,17 @@ import {
 } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
 import TruckAnimation from "./TruckAnimation"; // I
-
+import { clearCart } from "../../actions/cartActions";
 const SuccessOrder = () => {
-    console.log("Clearing sessionStorage");
-    sessionStorage.clear();
+    // console.log("Clearing sessionStorage");
+    // sessionStorage.clear();
 
-    console.log("Clearing localStorage");
-    localStorage.clear();
+    // console.log("Clearing localStorage");
+    // localStorage.clear();
+
+    const dispatch = useDispatch();
+
+    dispatch(clearCart())
     return (
         <div className="bg-zinc-100 min-h-screen flex justify-center items-center">
             <div className="bg-white p-4 justify-center flex flex-col items-center rounded-xl space-y-5 mb-20">
