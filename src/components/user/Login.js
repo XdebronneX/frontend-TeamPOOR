b2ebf79e4ae462383634290719cb68e8560e1233
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-05-05 22:07:09 +0800

    Design button

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -132,6 +132,9 @@ const Login = () => {
                                                     _hover={{
                                                         bg: 'blue.700',
                                                     }}
+                                                    isLoading={loading}
+                                                    loadingText="Signing in..."
+                                                    disabled={loading}
                                                     >
                                                 Sign in
                                             </Button>
