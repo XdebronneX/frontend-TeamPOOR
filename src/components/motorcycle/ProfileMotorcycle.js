9164274f8b448ddd3103869e4a53577ddeb3ee61
Reigne <monteronaelija@yahoo.com>
2024-05-06 00:11:50 +0800

    enhance design

--- src/components/motorcycle/ProfileMotorcycle.js
+++ src/components/motorcycle/ProfileMotorcycle.js
@@ -1,9 +1,9 @@
-import React, { useEffect, Fragment } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { myMotorcycle } from '../../actions/motorcycleActions';
-import { MDBDataTable } from 'mdbreact';
-import Loader from '../layout/Loader';
+import React, { useEffect, Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { myMotorcycle } from "../../actions/motorcycleActions";
+import { MDBDataTable } from "mdbreact";
+import Loader from "../layout/Loader";
 import {
   Box,
   Container,
@@ -21,7 +21,9 @@ import { FaPlusCircle } from "react-icons/fa";
 const ProfileMotorcycle = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { userMotorcycles, loading } = useSelector((state) => state.myMotor);
+  const { userMotorcycles, error, loading } = useSelector(
+    (state) => state.myMotor
+  );
 
   useEffect(() => {
     dispatch(myMotorcycle());
@@ -40,24 +42,24 @@ const ProfileMotorcycle = () => {
         //   sort: 'asc',
         // },
         {
-          label: 'Date Registered',
-          field: 'date',
-          sort: 'asc',
+          label: "Date Registered",
+          field: "date",
+          sort: "asc",
         },
         {
-          label: 'Plate Number',
-          field: 'plateNumber',
-          sort: 'disabled',
+          label: "Plate Number",
+          field: "plateNumber",
+          sort: "disabled",
         },
         {
-          label: 'Year',
-          field: 'year',
-          sort: 'disabled',
+          label: "Year",
+          field: "year",
+          sort: "disabled",
         },
         {
-          label: 'Brand',
-          field: 'brand',
-          sort: 'disabled',
+          label: "Brand",
+          field: "brand",
+          sort: "disabled",
         },
         // {
         //   label: 'Engine Number',
@@ -65,19 +67,19 @@ const ProfileMotorcycle = () => {
         //   sort: 'asc',
         // },
         {
-          label: 'Type',
-          field: 'type',
-          sort: 'disabled',
+          label: "Type",
+          field: "type",
+          sort: "disabled",
         },
         {
-          label: 'Fuel',
-          field: 'fuel',
-          sort: 'disabled',
+          label: "Fuel",
+          field: "fuel",
+          sort: "disabled",
         },
         {
-          label: 'Motorcycle',
-          field: 'imageMotorcycle',
-          sort: 'disabled',
+          label: "Motorcycle",
+          field: "imageMotorcycle",
+          sort: "disabled",
         },
         // {
         //   label: 'Plate Number Image',
@@ -90,11 +92,14 @@ const ProfileMotorcycle = () => {
 
     if (userMotorcycles) {
       userMotorcycles.forEach((row) => {
-        const formattedDate = new Date(row.createdAt).toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: '2-digit',
-        });
+        const formattedDate = new Date(row.createdAt).toLocaleDateString(
+          "en-US",
+          {
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+          }
+        );
         data.rows.push({
           // id: row._id,
           date: formattedDate,
@@ -108,13 +113,13 @@ const ProfileMotorcycle = () => {
             <img
               src={row.imageMotorcycle.url}
               alt="Motorcycle Image"
-              style={{ width: '100px', height: '100px' }}
+              style={{ width: "100px", height: "100px" }}
             />
           ) : (
             <img
               src="your_default_image_url"
               alt="Default Image"
-              style={{ width: '100px', height: '100px' }}
+              style={{ width: "100px", height: "100px" }}
             />
           ),
           // imagePlateNumber: row.imagePlateNumber ? (
@@ -138,32 +143,38 @@ const ProfileMotorcycle = () => {
   };
 
   return (
-    <Container maxW="container.xl">
-      <Flex justifyContent="space-between" alignItems="center" mb={4}>
-        <Heading as="h1" fontSize="3xl" fontWeight="bold">
-          My Motorcycles
-        </Heading>
-        <Button onClick={handleMotorcycle} colorScheme="teal" leftIcon={<FaPlusCircle />}>
-          Register Motorcycle
-        </Button>
-      </Flex>
-      {loading ? (
-        <Loader />
-      ) : (
-        <Fragment>
-          {userMotorcycles && userMotorcycles.length === 0 ? (
-            <Text>No motorcycles found.</Text>
-          ) : (
-            <MDBDataTable
-              striped
-              bordered
-              noBottomColumns
-              data={setMotors()}
-            />
-          )}
-        </Fragment>
-      )}
-    </Container>
+    <div className="flex-1 h-screen bg-zinc-100">
+      <div className="container pt-5">
+        <Flex justifyContent="space-between" alignItems="center" mb={4}>
+          <Heading as="h1" fontSize="3xl" fontWeight="bold">
+            My Motorcycles
+          </Heading>
+          <Button
+            onClick={handleMotorcycle}
+            colorScheme="teal"
+            leftIcon={<FaPlusCircle />}
+          >
+            Register Motorcycle
+          </Button>
+        </Flex>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="bg-white p-3 rounded">
+            {userMotorcycles && userMotorcycles.length === 0 ? (
+              <Text>No motorcycles found.</Text>
+            ) : (
+              <MDBDataTable
+                striped
+                bordered
+                noBottomColumns
+                data={setMotors()}
+              />
+            )}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
