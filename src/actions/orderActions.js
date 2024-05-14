007d9dc4f35c460df0ee7e9ce799bf45f3570ef1
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-05-15 02:43:54 +0800

    Fixing error

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -54,7 +54,7 @@ export const createOrder = order => async (dispatch, getState) => {
 
         dispatch({
             type: CREATE_ORDER_SUCCESS,
-            payload: data
+            payload: data.orderObject
         })
     } catch (error) {
         dispatch({
